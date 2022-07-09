fc126152235899338a088daa24fda4cc450d4dca
TalMat <TalinMatteson@gmail.com>
2022-07-09 11:50:27 -0500

    fill-aray - add native implementation

--- fill-array/index.ts
+++ fill-array/index.ts
@@ -1,9 +1,32 @@
 import { times } from "lodash";
 
-const data = [{ name: "tom"}, { name: "jerry" }];
-const defaultObj = { name: "" };
+const padArray = <T>(arr: T[], defaultVal: T, length: number) => {
+    const output: T[] = [];
+    for (let i = 0; i < length; i++)
+        output.push(arr[i] || (typeof defaultVal === "object" ? { ...defaultVal } : defaultVal));
+    return output;
+}
+
 const itemCount = 10;
 
-const output = times(itemCount, (i) => data[i] || defaultObj);
 
-console.log(output);
+
+const primitiveData = ["tom", "jerry"];
+const primitiveDefault = "";
+
+const nativeOutputPrimitive = padArray(primitiveData, primitiveDefault, itemCount);
+console.log(nativeOutputPrimitive);
+
+const lodashOutputPrimitive = times(itemCount, (i: number) => primitiveData[i] || primitiveDefault);
+console.log(lodashOutputPrimitive);
+
+
+
+const objData = [{ name: "tom"}, { name: "jerry" }];
+const objDefault = { name: "" };
+
+const nativeOutputObj = padArray(objData, objDefault, itemCount);
+console.log(nativeOutputObj);
+
+const lodashOutputObj = times(itemCount, (i: number) => objData[i] || objDefault);
+console.log(lodashOutputObj);
